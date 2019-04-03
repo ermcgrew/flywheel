8bb02297b6c7311bb4ee6457daf4a4a089d88e35
gholder <gaylord.holder@gmail.com>
2019-04-03 14:36:48 -0400

    minimally functioning gear

---
+++ gears/ashsharpicv/make_report.sh
@@ -0,0 +1,151 @@
+#!/bin/bash -x
+
+set -x -e 
+
+export PATH=$FLYWHEEL:$PATH
+
+# ------------------------------------
+# Generate a clinical report
+# ------------------------------------
+
+# Print usage by default
+if [[ $# -lt 1 || $1 == "h" || $1 == "-help" || $1 == "--help" ]] ; then
+  cat << USAGETEXT
+    generate_pdf_report.sh: Script to create a pdf clinical report
+    usage: 
+      generate_pdf_report.sh [options]
+    options:
+      -s  <str>         : Session ID number
+      -n  <str>         : Scan number
+      -t  <str>         : Ticket number
+      -a  <years>       : Patient's age
+      -w  <dir>         : Location of the work directory where the output should be generated
+USAGETEXT
+  exit 0
+fi 
+
+# Read the options
+while getopts "i:s:n:t:a:c:h:w:" opt; do
+    #echo "getopts found option $opt"
+    case $opt in
+
+	s) 
+	    session_id=$OPTARG
+	    #echo "$OPTARG"
+	    ;;
+	n)
+            scan_id=$OPTARG
+            #echo "$OPTARG"
+            ;;
+	t) 
+	    ticket=$OPTARG
+	    #echo "$OPTARG"
+	    ;;	
+	a) 
+	    age=$OPTARG
+	    #echo "$OPTARG"
+	    ;;
+	w)
+            OUTPUTDIR=$OPTARG
+            echo "$OPTARG"
+            ;;
+	
+	i)
+            INPUTDIR=$OPTARG
+            echo "$OPTARG"
+            ;;
+	
+	\?) echo "Unknown option $OPTARG"; exit 2;;
+
+	:) echo "Option $OPTARG requires an argument"; exit 2;;
+
+    esac
+done
+
+# Locate the input files and set the output path 
+rmdf=$INPUTDIR/clinical_report.Rmd
+ndf=$INPUTDIR/ADNI_metadata_for_R.csv
+
+full_id=${session_id}_${scan_id}
+report=${full_id}_report.pdf
+tex=${full_id}_report.tex
+
+# Get the HARP and ICV segmentation files from the workdirectory
+icv_layer=$INPUTDIR/${full_id}_icv.nii.gz
+harp_layer=$INPUTDIR/${full_id}_harp.nii.gz
+
+# Generate the ICV volumes out of the segmentation file
+extent=$(c3d $icv_layer -dup -lstat | awk -v id=101 '$1 == id {print $10}')
+if [[ $extent ]] ; then
+        icv=$(c3d $icv_layer -dup -lstat | awk -v id=101 '$1 == id {print $7}')
+	echo "$session_id ICV $extent $icv" > $OUTPUTDIR/${full_id}_icv_volumes.txt
+fi 
+
+# Generate the HARP Left volumes out of the segmentation file
+extent=$(c3d $harp_layer -dup -lstat | awk -v id=102 '$1 == id {print $10}')
+if [[ $extent ]] ; then
+	hvl=$(c3d $harp_layer -dup -lstat | awk -v id=102 '$1 == id {print $7}')
+        echo "$session_id HVL $extent $hvl" > $OUTPUTDIR/${full_id}_harp_left_volumes.txt
+fi
+
+# Generate the HARP Right volumes out of the segmentation file
+extent=$(c3d $harp_layer -dup -lstat | awk -v id=103 '$1 == id {print $10}')
+if [[ $extent ]] ; then
+	hvr=$(c3d $harp_layer -dup -lstat | awk -v id=103 '$1 == id {print $7}')
+        echo "$session_id HVR $extent $hvr" > $OUTPUTDIR/${full_id}_harp_right_volumes.txt
+fi
+
+# Get the HARP and ICV segmentation files from source directories
+#icv=$(cat $ICVDIR/final/${ticket}_left_corr_usegray_volumes.txt| awk -F' ' '{ print $5 }')
+#hvl=$(cat $HARPDIR/final/${ticket}_left_corr_usegray_volumes.txt| awk -F' ' '{ print $5 }')
+#hvr=$(cat $HARPDIR/final/${ticket}_right_corr_usegray_volumes.txt| awk -F' ' '{ print $5 }')
+
+# Get the QA files from source directories
+qaicv=$INPUTDIR/multiatlas_corr_nogray_icv_qa.png
+qahvl=$INPUTDIR/bootstrap_corr_nogray_harp_left_qa.png
+qahvr=$INPUTDIR/bootstrap_corr_nogray_harp_right_qa.png
+
+wget $(echo "$(itksnap-wt -dss-tickets-log $ticket | grep ICV | grep multiatlas | grep nogray | grep left | awk '{print $2}')") -O $qaicv
+wget $(echo "$(itksnap-wt -dss-tickets-log $ticket | grep HARP | grep bootstrap | grep nogray | grep left | awk '{print $2}')") -O $qahvl
+wget $(echo "$(itksnap-wt -dss-tickets-log $ticket | grep HARP | grep bootstrap | grep nogray | grep right | awk '{print $2}')") -O $qahvr
+
+echo "$OUTPUTDIR"
+
+# Cut the QA images to extract only interesting part
+qaicv_croped=$OUTPUTDIR/${full_id}_qa_icv.png
+qahvl_croped=$OUTPUTDIR/${full_id}_qa_harp_left.png
+qahvr_croped=$OUTPUTDIR/${full_id}_qa_harp_right.png
+
+w=$(c3d $qaicv -info-full | grep "Image Dimensions" | awk '{print $4}' | sed 's/[[,]//g')
+h=$(c3d $qaicv -info-full | grep "Image Dimensions" | awk '{print $5}' | sed 's/[[,]//g')
+s=$(python -c "print( ${h}/3 - ${h}*1/100 )")
+echo "$w $h $s"
+convert $qaicv -crop ${w}x${h}+0+$s $qaicv_croped
+
+w=$(c3d $qahvl -info-full | grep "Image Dimensions" | awk '{print $4}' | sed 's/[[,]//g')
+h=$(c3d $qahvl -info-full | grep "Image Dimensions" | awk '{print $5}' | sed 's/[[,]//g')
+s=$(python -c "print( ${h}/3 - ${h}*3/100 )")
+echo "$w $h $s"
+convert $qahvl -crop ${w}x${h}+0+$s $qahvl_croped
+
+w=$(c3d $qahvr -info-full | grep "Image Dimensions" | awk '{print $4}' | sed 's/[[,]//g')
+h=$(c3d $qahvr -info-full | grep "Image Dimensions" | awk '{print $5}' | sed 's/[[,]//g')
+s=$(python -c "print( ${h}/3 - ${h}*3/100 )")
+echo "$w $h $s"
+convert $qahvr -crop ${w}x${h}+0+$s $qahvr_croped
+
+#convert $qahvl -crop 344x92+0+46 $qahvl_croped
+#convert $qahvr -crop 344x92+0+46 $qahvr_croped
+
+echo -e "RMarkdown file:\n$rmdf\n"
+echo -e "Patient:\n$id - $age\n"
+echo -e "Volumes:\n$icv - $hvl - $hvr \n" 
+echo -e "Normative Data:\n$ndf \n"
+echo -e "QA:\n $(basename $qaicv) \n $(basename $qahvl) \n $(basename $qahvr) \n"
+echo -e "QA:\n $(basename $qaicv_croped) \n $(basename $qahvl_croped) \n $(basename $qahvr_croped) \n"
+
+# Call R to create the report with correct parameters
+R -e "rmarkdown::render('$rmdf',output_file = '$report', params = list( PID = '$session_id', PAGE = $age, PICV = as.double($icv), PHVL = as.double($hvl), PHVR = as.double($hvr), NDF = '$ndf', QAICV = '$qaicv_croped', QAHVL = '$qahvl_croped', QAHVR = '$qahvr_croped'))"
+
+ls -l $INPUTDIR
+
---
+++ gears/ashsharpicv/run_pipeline.sh
@@ -0,0 +1,147 @@
+#!/bin/bash -x
+set -x -e
+
+# Print usage by default
+if [[ $# -lt 1 || $1 == "h" || $1 == "-help" || $1 == "--help" ]] ; then
+  cat << USAGETEXT
+
+	run_pipeline.sh: Script to run the whole pipeline. It sends a ticket for the
+                         HARP-ICV segmentation service and generates a pdf report 
+    	                 out of the results.
+    	usage: 
+      		run_pipeline.sh [options] [DICOMDIR]
+   	options:
+		-p  <str>         : Project's name
+		-s  <str>         : MR session ID number
+      		-n  <str>         : Scan ID number
+		-a  <years>       : Patient's age
+
+USAGETEXT
+  exit 0
+fi 
+
+# Read the options
+while getopts "p:s:n:a:" opt; do
+    #echo "getopts found option $opt"
+	case $opt in
+	
+        	p)
+			project=$OPTARG
+			echo "$project"
+			;;
+		s)
+			session_id=$OPTARG
+			echo "$session_id"
+        		;;
+		n)
+			scan_id=$OPTARG
+			echo "$scan_id"
+			;;
+		a)
+			age=$OPTARG
+			echo "$age"
+			;;
+
+		\?) echo "Unknown option $OPTARG"; exit 2;;
+
+		:) echo "Option $OPTARG requires an argument"; exit 2;;
+	esac
+done
+
+shift $(($OPTIND - 1))
+
+INPUTDIR="$1"
+OUTPUTDIR="$2"
+TMPDIR=$(mktemp -d /tmp/pipline-XXXXX)
+
+full_id=${session_id}_${scan_id}
+
+native_image=${full_id}_native_mri.nii.gz
+input_image=${full_id}_mri.nii.gz
+
+if [ ! -d $TMPDIR ] ; then mkdir -p $TMPDIR ; fi
+
+if [ -d $SOURCEDIR ] ; then 
+	c3d -dicom-series-list $INPUTDIR 
+	series_id=$(c3d -dicom-series-list $INPUTDIR | grep 2 | awk '{ print $NF }')
+	c3d -dicom-series-read $INPUTDIR $series_id -o $TMPDIR/$native_image
+else
+	echo "The folder $INPUTDIR doesn't exist."
+	exit 1
+fi
+
+# Check the image
+width=$(c3d $TMPDIR/$native_image -info-full | grep "Image Dimensions" | awk '{print $4}' | sed 's/[[,]//g')
+height=$(c3d $TMPDIR/$native_image -info-full | grep "Image Dimensions" | awk '{print $5}' | sed 's/[[,]//g')
+sx=$(c3d $TMPDIR/$native_image -info-full | grep "Voxel Spacing" | awk '{print $4}' | sed 's/[][,]//g')
+sy=$(c3d $TMPDIR/$native_image -info-full | grep "Voxel Spacing" | awk '{print $5}' | sed 's/[][,]//g')
+sz=$(c3d $TMPDIR/$native_image -info-full | grep "Voxel Spacing" | awk '{print $6}' | sed 's/[][,]//g')
+smax=$(python -c "print(max([$sx,$sy,$sz]))")
+smin=$(python -c "print(min([$sx,$sy,$sz]))")
+aspect_ratio_min=$(python -c "print( float($smin)/float($smax))")
+aspect_ratio_max=$(python -c "print( float($smax)/float($smin))")
+
+if (( $width > 40 )) && (( $height > 40 )) ; then 
+	if [[ $(echo "$aspect_ratio_max < 3" | bc) == 1 ]] && [[ $(echo "$aspect_ratio_min > 0.33" | bc) == 1 ]] ; then	
+		trim_script=./trim_neck.sh
+		MASKDIR=$TMPDIR/mask
+		INTERDIR=$TMPDIR/inter
+		if [ ! -d $MASKDIR ] ; then mkdir -p $MASKDIR ; fi  
+		if [ ! -d $INTERDIR ] ; then mkdir -p $INTERDIR ; fi
+		$trim_script -m $MASKDIR -w $INTERDIR $TMPDIR/$native_image $TMPDIR/$input_image
+	else 
+		echo "Wrong scan input: the aspect ratio of the image is < 0.33 or > 3."
+		exit 1
+	fi
+else
+	echo "Wrong scan input: the width or/and the height of image is inferior to 40."
+        exit 1	 
+fi 
+
+# Create input workspace
+input_workspace=${full_id}_input.itksnap
+itksnap-wt -laa $TMPDIR/$input_image -ta T1 -psn "MRI" -ll -o $TMPDIR/$input_workspace
+
+# Create ticket with the HARP-ICV service number
+service=ASHS-HarP
+ticket_create_out=$TMPDIR/${full_id}_ticket_info.txt
+
+if [ -f $ticket_create_out ] ; then rm $ticket_create_out ; fi
+touch $ticket_create_out
+chmod 755 $ticket_create_out
+
+itksnap-wt -i $TMPDIR/$input_workspace -dss-tickets-create $service > $ticket_create_out
+ticket_number=$(cat $ticket_create_out | grep "^2> " | awk '{print $2}')
+ticket_code=$(printf %08d $ticket_number)
+
+# Check the processing of the ticket
+sleep 30s
+itksnap-wt -dss-tickets-wait $ticket_number 7200
+sleep 30s
+
+# Rename result files
+itksnap-wt -dss-tickets-download $ticket_number $TMPDIR 
+ticket_workspace=$TMPDIR/ticket_${ticket_code}_results.itksnap
+xnat_workspace=$TMPDIR/${full_id}_results.itksnap
+
+mri_layer=$(itksnap-wt -i $ticket_workspace -ll | grep MRI | awk '{print $2}')
+icv_layer=$(itksnap-wt -i $ticket_workspace -ll | grep ICV | awk '{print $2}')
+harp_layer=$(itksnap-wt -i $ticket_workspace -ll | grep HARP | awk '{print $2}')
+
+itksnap-wt -i $ticket_workspace \
+	        -layers-pick $mri_layer -props-rename-file $TMPDIR/${full_id}_mri.nii.gz \
+                -layers-pick $icv_layer -props-rename-file $TMPDIR/${full_id}_icv.nii.gz \
+	        -layers-pick $harp_layer -props-rename-file $TMPDIR/${full_id}_harp.nii.gz \
+		-o $xnat_workspace
+
+cp "$ticket_workspace" "${TMPDIR}/${full_id}"_{mir,icv,harp}_nii.gz "$OUTPUTDIR"
+cp clinical_report.Rmd ADNI_metadata_for_R.csv "$TMPDIR"
+
+make_report.sh -s $session_id \
+			  -n $scan_id \
+			  -a $age \
+			  -t $ticket_code \
+			  -i "$TMPDIR" \
+			  -w "$OUTPUTDIR"
+
+itksnap-wt -dss-ticket-delete "$ticket_number"
