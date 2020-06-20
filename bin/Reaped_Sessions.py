57a84a679c3e635821f5628f5dec0f78b963f2f6
gholder <gaylord.holder@gmail.com>
2020-06-20 17:42:26 -0400

    WIP

--- bin/Reaped_Sessions.py
+++ bin/Reaped_Sessions.py
@@ -4,6 +4,8 @@
 import csv
 import flywheel
 import fwgearutils
+import json
+import os
 import sys
 from pprint import pprint
 
@@ -13,33 +15,51 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--month', action='store', nargs=1, type=str, help='Month number, 1 = January, 12 = December')
 parser.add_argument('-y', '--year', action='store', nargs=1, type=str, default=[2020], help='Year number')
-parser.add_argument('out', type=argparse.FileType('w', encoding='UTF-8'))
+parser.add_argument('out', nargs="*", help='output file or stdout by default')
 args = parser.parse_args()
 
 fw = fwgearutils.getFW(args)
 project_data = []
 
+acquisitions = []
+
 for project in fw.get_all_projects(exhaustive=True):
-    print('Counting project {}'.format(project.label), file=sys.stderr)
+
     reaper_count_hash = {}
     query = 'files.origin.type=device,parents.project={}'.format(project.id)
-    for a in fw.acquisitions.find(query, exhaustive=True):
+    query = 'parents.project={}'.format(project.id)
+    '''
+    for a in 
         reaper_count_hash[a.session] = 1
-            
+        acquisitions.append(a)
+    '''
+
+    origin = {'id': None, 'type': None}
+    a = fw.acquisitions.find(query, exhaustive=True)
+    if (len(a)):
+        try:
+            origin = { 'id': a[0].files[0].origin.id, 'type': a[0].files[0].origin.type }
+            print("{} from {}".format(a[0].label,origin), file=sys.stderr)
+        except (IndexError) as e:
+            print("{} has no files".format(a[0].label),  file=sys.stderr)
+
     query = 'parents.project={}'.format(project.id)
     sessions = fw.sessions.find(query, exhaustive=True, limit=10000)
     for session in sessions:
         project_data.append({
+            'session_created': session.created,
             'group_id': project.group,
             'project_label': project.label,
             'project_id': project.id,
-            'reaped_session': session.id in reaper_count_hash.keys(),
-            'session_created': session.created
+            'session_id': session.id,
+            'origin_id': origin['id'],
+            'origin_type': origin['type'],
         })
 
-print('writing to session_counts.csv')
-with open('session_counts.csv', 'w') as fp:
-    writer = csv.DictWriter(fp, ['group_id', 'project_id', 'project_label', 'reaped_session', 'session_created','session_analyses'])
+with (os.fdopen(os.dup(sys.stdout.fileno()), 'w')
+      if (len(args.out) == 0)
+      else open(args.out[0], 'w')) as fp:
+    writer = csv.DictWriter(fp, ['session_created', 'group_id', 'project_label', 'project_id', 'session_id', 'origin_id', 'origin_type' ])
     writer.writeheader()
     for row in project_data:
         writer.writerow(row)
