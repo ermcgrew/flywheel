d629509258e4317df3facc7431c288969118f840
PlanetM <ermcgrew@users.noreply.github.com>
2022-09-06 10:28:06 -0400

    indd and date variables

--- naccsc/rename.py
+++ naccsc/rename.py
@@ -1,4 +1,5 @@
 import flywheel
+#user must be logged into flywheel via CLI to use flywheel.client()
 fw = flywheel.Client()
 
 #select project by project ID
@@ -10,16 +11,15 @@ except flywheel.ApiException as e:
 
 #create list of sessions
 #####use a filter here if changing PET to match MRI so only sessions not matching correct format are pulled
-#subset to test on, incl MRI & PET, remove created filter for actual use
 try:
-    sessions = project.sessions.find('created>2022-07-19') 
+    sessions = project.sessions.find('created>2022-07-19') #subset to test on, incl MRI & PET, remove created filter for actual use
 except flywheel.ApiException as e:
     print(f'Error: {e}')
 
 print(f'{len(sessions)} sessions in project {project.label}')
 
 for count, session in enumerate(sessions, 1):
-    print(f'loop {count}: {session.label}')
+    # print(f'loop {count}: {session.label}')
     # for acquisition in session.acquisitions():
     #     print('in acq loop')
         #if acquisition.files[0].modality contains MR (vs contains PT)
@@ -27,15 +27,18 @@ for count, session in enumerate(sessions, 1):
         #if session.label not SubjectIDxYYYYMMDDxScanType
         #########how to ID incorrect session labels? 
         # if session.label != "??????x????????x??": 
-            # print(session.label)
-            
-            #subject.code or subject.label for SubjectID
-            # indd = session.subject.label
-            #date = session.created (datetime conversion)
-            #scantype = acquisition file name (parse)
-            #save as session.label
-            #session.label = indd + 'x' + 'date' + 'x' + 'scantype'
-            #record of change--print + log when running script
+    print(f'Session label: {session.label} is incorrect, renaming...')
+    indd = session.subject.label
+    print(f'Subject indd: {indd}')
+    date = str(session.created)[:10].replace('-','')
+    print(f'Session date: {date}')
+    
+    #scantype = acquisition file name (parse)
+
+    newlabel = indd + 'x' + date +'x'
+    print(f'Renaming session to: {newlabel}')
+
+
     #if PET 
         #if session.label not SubjectIDxScanTypexYYYYMMDD
         # if session.label != "??????xPETx????????": 
