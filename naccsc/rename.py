86fc729e6a2b5254e0489e4e9eba7d5630a85568
PlanetM <ermcgrew@users.noreply.github.com>
2022-09-06 11:08:57 -0400

    changed to iter_find for faster runtime

--- naccsc/rename.py
+++ naccsc/rename.py
@@ -12,31 +12,44 @@ except flywheel.ApiException as e:
 #create list of sessions
 #####use a filter here if changing PET to match MRI so only sessions not matching correct format are pulled
 try:
-    sessions = project.sessions.find('created>2022-07-19') #subset to test on, incl MRI & PET, remove created filter for actual use
+    sessions = project.sessions.iter_find('created>2022-07-19') #subset to test on, incl MRI & PET, remove created filter for actual use
 except flywheel.ApiException as e:
     print(f'Error: {e}')
 
-print(f'{len(sessions)} sessions in project {project.label}')
-
 for count, session in enumerate(sessions, 1):
-    # print(f'loop {count}: {session.label}')
+    print(f'session loop {count}: {session.label}')
     # for acquisition in session.acquisitions():
     #     print('in acq loop')
+    #     print(f'{acquisition.files[0].modality}')
+    #     if acquisition.files[0].modality == 'MR':
+    #         print('MR found')
+        
         #if acquisition.files[0].modality contains MR (vs contains PT)
+
+
+
     #if MRI 
         #if session.label not SubjectIDxYYYYMMDDxScanType
         #########how to ID incorrect session labels? 
         # if session.label != "??????x????????x??": 
-    print(f'Session label: {session.label} is incorrect, renaming...')
-    indd = session.subject.label
-    print(f'Subject indd: {indd}')
-    date = str(session.created)[:10].replace('-','')
-    print(f'Session date: {date}')
-    
-    #scantype = acquisition file name (parse)
 
-    newlabel = indd + 'x' + date +'x'
-    print(f'Renaming session to: {newlabel}')
+
+#############renaming block
+    # print(f'Session label: {session.label} is incorrect, renaming...')
+    # indd = session.subject.label
+    # print(f'Subject indd: {indd}')
+    # date = str(session.created)[:10].replace('-','')
+    # print(f'Session date: {date}')
+    
+    # #scantype = acquisition file name (parse)
+
+    # newlabel = indd + 'x' + date + 'x'
+    # print(f'Renaming session to: {newlabel}')
+
+#############################################################
+    # session.update(label = newlabel) #in dictionary? test with unsorted first
+    # print(f'Session label is now {session.label}')
+#############################################################
 
 
     #if PET 
@@ -44,5 +57,5 @@ for count, session in enumerate(sessions, 1):
         # if session.label != "??????xPETx????????": 
             #repeat steps from MRI with order change--unless we switch so they match
 
-print(f'{count} incorrect session names')
+print(f'{count} sessions in project {project.label} checked')
         
