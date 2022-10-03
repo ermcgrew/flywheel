6e5c40649d978adc28762a2f86a3020c85c7687c
ermcgrew <ermcgrew@users.noreply.github.com>
2022-10-03 15:27:52 -0400

    saving work

--- naccsc/test_update.py
+++ naccsc/test_update.py
@@ -1,10 +1,35 @@
 import flywheel
 fw = flywheel.Client()
 
-session = fw.get('631a35c680fcd66d1fe77a21')
+session = fw.get('6304f0e9d1eb12db471e2cb4')
 print(session.label)
+if '_' in session.subject.label: 
+    indd=session.subject.label.replace('_',"x")
+else: 
+    indd = session.subject.label
+    
+date = str(session.timestamp)[:10].replace('-','')
 
-newlabel = '125111x20220829xPI2620PETxABC'
+labellist=[acquisition.label for acquisition in session.acquisitions()]
+if 'Axial MB DTI' in labellist:
+    study='LEADS'
+elif 'LLASL_m16LC_3.0s_2.0s_bs31_mis' in labellist:
+    study='DVCID'
+
+for acquisition in session.acquisitions():
+    if acquisition.label == "PhoenixZIPReport" or acquisition.label == "Exam Summary_401_401":
+        continue
+    else:
+        acquisition = acquisition.reload() 
+        if acquisition.files[0].type == 'dicom':
+            f = acquisition.files[0].info 
+            magstrength=f['MagneticFieldStrength']
+            if magstrength == 3:
+                scantype = '3T'
+        break
+
+newlabel = indd + 'x' + date + 'x' + scantype + 'x' + study
+print(f'Renaming session to: {newlabel}') 
 
 session.update({'label':newlabel})
 print(f'Session name updated to: {session.label}')
