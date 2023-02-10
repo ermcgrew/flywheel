9534d67f353e24de040f5bbd7cbd3fc2e456912e
ermcgrew <ermcgrew@users.noreply.github.com>
2023-02-10 11:42:34 -0500

    check_correct function written

--- naccsc/check_new_session_names.py
+++ naccsc/check_new_session_names.py
@@ -2,65 +2,64 @@
 
 import fwgearutils
 import flywheel
-import sys 
-import os 
+import sys
+import os
 
-if __name__ == '__main__':
+
+def check_correct(sessionlabellist, subject, date):
+    check = [False, False, False, False]
+    # for x in range(0,len(sessionlabellist)):
+
+    if len(sessionlabellist) == 4:
+        if sessionlabellist[0] == subject:
+            check[0] = True
+
+        if sessionlabellist[1] == date:
+            check[1] = True
+
+        if sessionlabellist[2] in scantypelist:
+            check[2] = True
+
+        if sessionlabellist[3] in studylist:
+            check[3] = True
+
+    return check
+
+
+if __name__ == "__main__":
     CmdName = os.path.basename(sys.argv[0])
-    
-    fw = fwgearutils.getFW('test')
-    if (not fw):
+
+    fw = fwgearutils.getFW("test")
+    if not fw:
         print(f"{CmdName}: unable to initialize flywheel object", file=sys.stderr)
         sys.exit(1)
 
+    # get NACC-SC flywheel project
     try:
-        project = fw.get_project('5c508d5fc2a4ad002d7628d8')
+        project = fw.get_project("5c508d5fc2a4ad002d7628d8")
     except flywheel.ApiException as e:
-        print(f'Error: {e}')    
-    
-    #create list of sessions
+        print(f"Error: {e}")
+
+    # get list of sessions
     try:
         sessions = project.sessions.iter_find("created>2022-12-01")
     except flywheel.ApiException as e:
-        print(f'Error: {e}')
+        print(f"Error: {e}")
 
-    scantypelist=["3T", "7T", "PI2620PET", "FBBPET", "AV1451PET", "FDGPET"]
-    studylist=["ABC", "ABCD2", "VCID", "LEADS", "YMTL"]
-    
+    # accepted options for scantypes and studies
+    scantypelist = ["3T", "7T", "PI2620PET", "FBBPET", "AV1451PET", "FDGPET"]
+    studylist = ["ABC", "ABCD2", "VCID", "LEADS", "YMTL"]
+
+    # review each session
     for session in sessions:
-        print(f"{session.label}")
-        
-        # break into pieces 
-        sessionlabellist=session.label.rsplit('x',3)
-        check=[False,False,False,False]
+        # print(f"{session.label}")
 
-        if len(sessionlabellist) == 4:
-            if sessionlabellist[0] == session.subject.label:
-                check[0]=True
-            
-            if sessionlabellist[1] == str(session.timestamp)[:10].replace('-',''):
-                check[1]=True
+        sessionlabellist = session.label.rsplit("x", 3)
+        subject = session.subject.label
+        date = str(session.timestamp)[:10].replace("-", "")
 
-            if sessionlabellist[2] in scantypelist:
-                check[2]=True    
-
-            if sessionlabellist[3] in studylist:
-                check[3]=True    
-            
-            # if all check items are True
-                # continue
-            # else:
-                # print("needs renaming")
+        if check_correct(sessionlabellist, subject, date) == [True, True, True, True]:
+            print(f"{session.label} is correct")
+            continue
         else:
-            print('needs renaming')
-            
-
-
-# if first piece == subject.session.label
-# if second piece == timestamp
-# if third piece in scantypelist 3T 7T PI2620PET FBBPET AV1451PET FDGPET
-# if last piece in study suffix list ABC, ABCD2, VCID, LEADS, YMTL
-
-# if all true
-# then true
-# else go to rename function
+            print(f"{session.label} needs to be renamed")
