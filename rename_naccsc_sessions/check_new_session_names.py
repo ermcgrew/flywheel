df3307cbad50fdb6988c356f0b6762d1f7b89787
PlanetM <ermcgrew@users.noreply.github.com>
2023-02-28 11:51:58 -0500

    moved info from readme to block comment at top of script

--- rename_naccsc_sessions/check_new_session_names.py
+++ rename_naccsc_sessions/check_new_session_names.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
 
+"""
+This script runs weekly via cron job set up by Emily on bscsub cluster. 
+It checks that new flywheel session names have the correct format and fixes them, if enough information is present. 
+Log is emailed to Emily, who manually fixes any sessions with unknown information.
+
+Fuctions:
+- main()
+  - check_correct(sessionlabellist, subject, date)
+  - rename_session(session, subject, date)
+- email_log(filepath)
+- parse_log(filepath)
+
+Debugging levels
+- Debug: correct session label
+- Info: renamed session label
+- Warning: incorrectly formatted subject label OR insufficient information for full renaming
+
+detailed logs of each weekly run are saved at {file location tbd...}/log_check_new_session_names_{datetime}.txt
+- get list of renamed sessions with: `cat log_check_new_session_names_{datetime}.txt | grep INFO | cut -d ":" -f 3,4`  
+- get list of items needing attention with: `cat log_check_new_session_names_{datetime}.txt | grep WARNING`
+
+log of all session names changed by this script, in the format old name:new name:date changed, are saved at 
+{file location tbd...}/all_fw_session_renames.txt
+"""
+
 import fwgearutils
 import flywheel
 import logging
